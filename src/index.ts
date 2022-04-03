021c671121f328d6d521577975c1553f49372f9c
Ankit <ankitdevenderkhosla@airasia.com>
2022-04-03 11:45:43 +0530

    aded date and dateTime scalar types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import { GraphQLDate, GraphQLDateTime } from 'graphql-iso-date';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import resolvers from './resolvers';
+
+const allResolvers = {
+  ...resolvers,
+  Date: GraphQLDate,
+  DateTime: GraphQLDateTime
+};
 
 const schemaPublic: string = fs
   .readFileSync(path.resolve(__dirname, './schema/schema.graphql'))
   .toString('utf8');
 
 export const schema: any = makeExecutableSchema({
-  resolvers,
+  resolvers: allResolvers,
   typeDefs: [schemaPublic]
 });
